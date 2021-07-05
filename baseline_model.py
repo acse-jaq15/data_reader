80c042fceaf05a65fd915d35d6f7c8c3752d6683
acse-jaq15 <72440238+acse-jaq15@users.noreply.github.com>
2021-07-05 14:11:35 +0100

    Update baseline_model.py

--- baseline_model.py
+++ baseline_model.py
@@ -1,17 +1,16 @@
-"""Module containing a baseline model"""
+"""Module containing a base model"""
 import numpy as np
 
 
-class Baseline_Model:
-    """Class to perform baseline modelling"""
+class Base_Model:
+    """Class to perform base/dummy modelling"""
 
     def __init__(self, input_data, window_horizon):
         """
-        Initialiser for Data_Reader class to read data in .csv format
+        Initialiser for Base_Model class
 
         Parameters
         ----------
-
             input_data (array):
                 array or array like X values to generate predictions
 
@@ -20,7 +19,7 @@ class Baseline_Model:
 
         Example
         -------
-            Data_Reader('Al', 2019)
+            Base_Model(X_test, 30)
         """
 
         self.input_data = input_data
@@ -28,28 +27,35 @@ class Baseline_Model:
 
     def predict_y(self, test_len):
         """
-        Method to predict y from X, where y is the mean
-        of window_horizon time steps of X
+        Method to predict y from X, where y is the mean of window_horizon time
+        steps of X
 
         Parameters
         ----------
-            None
+            test_len (int):
+                length of test dataset, equivalent to number of predictions to
+                be made
 
         Example
         -------
-            d_reader.normalise()
-            d_reader.norm_price
+            Base_Model.predict_y(150)
 
         Returns
         -------
             y_pred as class attribute
         """
 
+        # initial definition of y_pred attribute as empty list
         self.y_pred = []
+
+        # converting .input_data attribute to an array with 2 dimensions
         X_data = np.array(self.input_data, ndmin=2)
 
+        # iterating through X_data and appending predicted values to y_pred
         for i in range(0, test_len):
             self.y_pred.append(np.mean(X_data[i]))
 
+        # converting y_pred to an array with 2 dimensions
         self.y_pred = np.array(self.y_pred, ndmin=2)
+        # reshaping to enforce shape constraints
         self.y_pred = np.reshape(self.y_pred, (test_len, 1))
